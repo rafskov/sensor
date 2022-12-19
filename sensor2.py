f525d93d4c4c582f9a07c2f46092c6402909cdaa
Rafael Skovron <rafskov@github.com>
2022-12-19 05:26:45 +0000

    changed to adafruit

--- sensor2.py
+++ sensor2.py
@@ -1,21 +1,20 @@
-#create a function grab temperature and humidity from the DHT12 sensor
-def getTempHumid():
-    """gets a reading from the DHT12 sensor"""
-    #read the sensor
-    result = dht12.read()
-    #check if we got a good reading
-    if result.is_valid():
-        #if we got a good reading then return the temperature and humidity
-        return result.temperature, result.humidity
-    else:
-        #if we did not get a good reading then return None for both temperature and humidity
-        return None, None
 
-#log temperature and humidity to a file
-def logTempHumid(temperature, humidity):
-    """logs the temperature and humidity to a file"""
-    #open the file
-    with open("temp_humidity.txt", "a") as f:
-        #write the temperature and humidity to the file
-        f.write("{0},{1}")
+#use ADAFRUIT DHT12 sensor
+import adafruit_dht
+
+#create function using ADAFRUIT DHT12 to get temperature and humidity
+def get_temp_humid():
+    dhtDevice = adafruit_dht.DHT12(board.D4)
+    temperature = dhtDevice.temperature
+    humidity = dhtDevice.humidity
+    return temperature, humidity
+
+#create function to log temperature and humidity to file
+def log_temp_humid():
+    temperature, humidity = get_temp_humid()
+    with open("sensor2.txt", "a") as f:
+        f.write("{},{}".format(temperature, humidity))
+
+
+
 
